f4ab19e6448175600172be18e67d311a35f8c34b
invisy <3dvlad2013@gmail.com>
2019-05-22 00:01:13 +0300

    DebugScript

--- AirportsTracking/AirportsTracking/Program.cs
+++ AirportsTracking/AirportsTracking/Program.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;  //ONLY FOR TEST
 
 namespace AirportsTracking
 {
     static class Program
     {
-        [DllImport("Kernel32.dll")] //ONLY FOR TEST
-        static extern Boolean AllocConsole(); //ONLY FOR TEST
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            testConsole.AllocConsole(); //ONLY FOR TEST
+            testConsole.MainDebug();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
